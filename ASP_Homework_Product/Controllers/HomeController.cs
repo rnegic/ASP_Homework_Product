99086104e66301fdf22fb871519b988272afaa99
rnegic <arnego666@gmail.com>
2024-03-10 14:53:56 +0300

    добавлен список в контроллер

--- ASP_Homework_Product/Controllers/HomeController.cs
+++ ASP_Homework_Product/Controllers/HomeController.cs
@@ -20,15 +20,20 @@ namespace ASP_Homework_Product.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "пицца", Cost = 450, Description = "вкуснейшая" },
+                new Product { Id = 2, Name = "таук", Cost = 300, Description = "сочный" },
+                new Product { Id = 3, Name = "паук", Cost = 990, Description = "паучок" }
+            };
+
+            return View(products);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
-
-        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
