193463eaee49946a22b204b8fded7d92f95dfe15
rnegic <arnego666@gmail.com>
2024-03-31 16:32:54 +0300

    Added CatalogController, View for Catalog, minimum bootstrap styles

---
+++ ASP_Homework_Product/Controllers/CatalogController.cs
@@ -0,0 +1,85 @@
+﻿using ASP_Homework_Product.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: CatalogController
+        public ActionResult Index()
+        {
+            var products = ProductList.GetProducts();
+            return View(products);
+        }
+
+        // GET: CatalogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CatalogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CatalogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatalogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CatalogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatalogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CatalogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
